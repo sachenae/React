8d60204c5a7c6822b92811f579fdbd2805683ce4
sachins <39sachen@gmail.com>
2018-05-19 12:59:23 +0300

    implemented search and dispaly list

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -9,9 +9,11 @@ class SearchBar extends Component {
     render() {
         return (
         <div>
-        <input onChange = {event => this.setState({term: event.target.value})} />
-        Value of input = {this.state.term}
+            <input 
+                value={this.state.term}
+                onChange={event => this.setState({term: event.target.value})} />
         </div>
-        )}
+        );
+    }
 }
 export default SearchBar;
--- src/index.js
+++ src/index.js
@@ -1,13 +1,33 @@
-import React from 'react'
+import React, { Component } from 'react'
 import ReactDOM from 'react-dom'
 import SearchBar from './components/search_bar'
+import YTSearch from 'youtube-api-search'
+import VideoList from './components/video_list'
 
 const API_KEY = 'AIzaSyAZ3ffQM4fIAMxEtgThCA0qnBa-OYJNKpE';
 
-const App = () => {
+YTSearch({key: API_KEY, term : 'surf'}, function(data){
+    console.log(data);
+});
+
+class App extends Component {
+    constructor(props){
+        super(props)
+
+        this.state = { vids: [] };
+
+        YTSearch({key: API_KEY, term : 'surf'}, (vids) => {
+           this.setState({vids});
+        });
+
+
+
+    }
+    render(){
     return <div>
         <SearchBar />
+        <VideoList videos = {this.state.vids} />
     </div>;
 };
-
+}
 ReactDOM.render(<App />, document.querySelector('.container'))
